e5ae9076721bb61c06752def918aba12a3658795
Fernando Sirni <fsirni@gmail.com>
2022-10-17 22:15:31 -0400

    transactions by contract

--- pages/token/index.tsx
+++ pages/token/index.tsx
@@ -1,8 +1,40 @@
+import { getDataFromTree } from '@apollo/client/react/ssr'
+import { get } from 'lodash'
 import type { NextPage } from 'next'
 import Link from 'next/link'
-import { Row, Col, Table } from 'react-bootstrap'
+import { useState } from 'react'
+import { Row, Col, Table, Pagination } from 'react-bootstrap'
+import { useGetTransactionsByContractQuery, GetTransactionsByContractQuery } from '../../generated'
+import { formatTimeAgo, showShortHash } from '../../lib/utils'
+import withApollo from '../../lib/withApollo'
 
 const Transaction: NextPage = () => {
+  const [pagination, setPagination] = useState({
+    skip: 0,
+    take: 5,
+    orderAsc: false,
+    address: '5HbiqNyWkVdVhLQQrU9ovLoXEHenEvzLNAXUmUEFwAD5c4Lg', // This shouldnt be here
+  })
+  const { data } = useGetTransactionsByContractQuery({ variables: pagination })
+  const transactions = get(
+    data,
+    'getTransactionsByContract',
+    [],
+  ) as GetTransactionsByContractQuery['getTransactionsByContract']
+
+  const toogleOrder = () => {
+    setPagination({ ...pagination, orderAsc: !pagination.orderAsc })
+  }
+
+  const nextPage = () => {
+    setPagination({ ...pagination, skip: pagination.skip + 10 })
+  }
+
+  const previousPage = () => {
+    const newSkip = pagination.skip - 10
+    setPagination({ ...pagination, skip: newSkip < 0 ? 0 : newSkip })
+  }
+
   return (
     <>
       <Row className="mb-5">
@@ -23,41 +55,50 @@ const Transaction: NextPage = () => {
         </Col>
       </Row>
       <Row>
-        <Col>
-          <Table responsive className="ink_table">
+        <Col xs="12">
+          <Table responsive hover className="ink_table">
             <thead>
               <tr>
-                <th>Block number</th>
-                <th>Block Height</th>
-                <th>Time</th>
-                <th>Mine by</th>
-                <th>Transactions</th>
-                <th>Size</th>
+                <th>Tx Hash</th>
+                <th>Block</th>
+                <th onClick={() => toogleOrder()}>Time</th>
+                <th>Method</th>
+                <th>Section</th>
+                <th>Signer</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td className="black">#15167764</td>
-                <td>15167764</td>
-                <td>Jul-18-2022 04:38:07 PM +UTC</td>
-                <td className="black">0xea674fdde714fd979de3edf0f56aa9716b898ec8 </td>
-                <td>4</td>
-                <td>540 bytes</td>
-              </tr>
-              <tr>
-                <td className="black">#15167764</td>
-                <td>15167764</td>
-                <td>Jul-18-2022 04:38:07 PM +UTC</td>
-                <td className="black">0xea674fdde714fd979de3edf0f56aa9716b898ec8 </td>
-                <td>4</td>
-                <td>540 bytes</td>
-              </tr>
+              {transactions.map((transaction) => (
+                <tr key={transaction.hash}>
+                  <td className="black">
+                    <Link href={'/transaction/details/' + transaction.hash}>{showShortHash(transaction.hash)}</Link>
+                  </td>
+                  <td className="black">
+                    <Link href={'/block/details/' + transaction.blockHash}>
+                      {showShortHash(transaction.blockHash || '')}
+                    </Link>
+                  </td>
+                  <td>
+                    {formatTimeAgo(new Date(transaction.timestamp)) + ' ago'} (
+                    {new Date(transaction.timestamp).toUTCString()})
+                  </td>
+                  <td>{transaction.method}</td>
+                  <td>{transaction.section}</td>
+                  <td className="black">{transaction.signer}</td>
+                </tr>
+              ))}
             </tbody>
           </Table>
+        </Col>
+        <Col xs="12" className="d-flex justify-content-center my-4">
+          <Pagination>
+            <Pagination.Prev onClick={() => previousPage()} />
+            <Pagination.Next onClick={() => nextPage()} />
+          </Pagination>
         </Col>
       </Row>
     </>
   )
 }
 
-export default Transaction
+export default withApollo(Transaction, { getDataFromTree })
