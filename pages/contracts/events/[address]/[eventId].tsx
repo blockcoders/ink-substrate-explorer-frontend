daa59b82957044b20ba8adeda50160f8fb79f326
naunyiciodeb <naunyiciodeb@github.com>
2022-10-24 11:46:51 -0300

    btn

--- pages/contracts/events/[address]/[eventId].tsx
+++ pages/contracts/events/[address]/[eventId].tsx
@@ -38,7 +38,7 @@ const Event: NextPage = () => {
             </h4>
           </Col>
           <Col className="mb-4" xs="2">
-            <button className="ink-button ink-button_violet mt-3" onClick={() => decode()}>
+            <button className="transaction-tabs_buttons transaction-tabs_buttons-btn transaction-tabs_buttons-btn_active text-rigth" onClick={() => decode()}>
               Decode Event
             </button>
           </Col>
